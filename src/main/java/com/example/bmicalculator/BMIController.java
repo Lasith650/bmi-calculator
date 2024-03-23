8a7b1f1e4988151aca2d3d7e35530f500955c326
Lasith Manaram Jayalath <lasith@Lasiths-MacBook-Air.local>
2024-03-23 22:12:53 +0530

    Add cross origin and request mapping

--- src/main/java/com/example/bmicalculator/BMIController.java
+++ src/main/java/com/example/bmicalculator/BMIController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api/bmi")
 public class BMIController
 {
   private final BMIRepository bmiRepository;
