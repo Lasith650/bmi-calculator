f1da9c1b5f4dfac661471037dfce2eb5222e9084
Lasith Manaram Jayalath <lasith@Lasiths-MacBook-Air.local>
2024-03-23 22:00:52 +0530

    Update controller

--- src/main/java/com/example/bmicalculator/BMIController.java
+++ src/main/java/com/example/bmicalculator/BMIController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
-@RequestMapping("/api/bmi")
 public class BMIController
 {
   private final BMIRepository bmiRepository;
