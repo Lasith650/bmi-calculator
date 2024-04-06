b226409d2c4e80af68426ab0f79061a96f19f7d5
Lasith Manaram Jayalath <lasith@Lasiths-MacBook-Air.local>
2024-04-06 23:49:08 +0530

    Add local host as the cross origin

--- src/main/java/com/example/bmicalculator/BMIController.java
+++ src/main/java/com/example/bmicalculator/BMIController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/bmi")
 public class BMIController
 {
